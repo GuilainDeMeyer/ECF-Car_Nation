2e0278a0b36b2cbc68c02faee46d4fa74a96b0ca
Guilain De Meyer <de.meyer.guilain@gmail.com>
2023-11-10 17:11:25 +0100

    Header&Footer avec responsive

--- index.php
+++ index.php
@@ -3,11 +3,60 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Car-Nation</title>
+    <title>Atelier Car-Nation : restauration de voitures anciennes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <?php
-    include_once <head></head>
-    ?>
+<!-- START Header -->
+    <header class="d-flex flex-wrap justify-content-between align-items-center py-3 mb-4 border-bottom navbar-dark bg-dark">
+        <div class="col-12 col-lg-4 justify-content-center">
+            <a href="index.php" class="d-flex align-items-center justify-content-center mb-3 mb-md-0 link-body-emphasis text-decoration-none">
+                <img src="Assets/Logo/Car-NationNoBG.png" alt="logoCN" width="80px">
+            </a>
+        </div>
+
+        <ul class="nav nav-pills col-12 col-lg-4 mb-2 justify-content-center align-items-center">
+            <li><a href="#" class="NavHeader nav-link active px-2">Présentation</a></li>
+            <li><a href="#" class="NavHeader nav-link px-2">Atelier</a></li>
+            <li><a href="#" class="NavHeader nav-link px-2">Galerie Photos</a></li>
+            <li><a href="#" class="NavHeader nav-link px-2">Recrutement</a></li>
+            <li><a href="#" class="NavHeader nav-link px-2">Nous contacter</a></li>
+        </ul>
+
+        <div class="col-12 col-lg-4 d-flex justify-content-center">
+            <button type="button" class="btn btn-outline-danger me-2">Login</button>
+            <button type="button" class="btn btn-danger">Sign-up</button>
+        </div>
+    </header>
+    <!-- END Header -->
+
+
+
+
+
+
+
+
+
+<!-- START footer -->
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="nav col-12 col-lg-4 justify-content-center">
+            <p class="Footertxt">© 2023 Car-Nation, Inc</p>
+        </div>
+
+        <a href="index.php" class="col-12 col-lg-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <img src="Assets/Logo/Car-NationNoBG.png" alt="logoCN" width="80px">
+        </a>
+
+        <div class="nav col-12 col-lg-4 justify-content-center">
+            <p><a href="index.php" class="Footertxt nav-link px-2 text-body-active">Nous contacter</a></p>
+        </div>
+    </footer>
+<!-- END Footer -->
+
+
+
 </body>
 </html>
